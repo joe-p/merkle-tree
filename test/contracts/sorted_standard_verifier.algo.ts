6b6bb86384d2671961ef76871e1583fd828c4521
Joe Polny <joepolny@gmail.com>
2024-05-24 16:51:39 -0400

    add comments

--- test/contracts/sorted_standard_verifier.algo.ts
+++ test/contracts/sorted_standard_verifier.algo.ts
@@ -1,6 +1,15 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 export default class SortedStandardVerifier extends Contract {
+  /**
+   * Prove that a leaf is in a merkle tree by verifying the path to the root
+   *
+   * @param root The merkle root
+   * @param leaf The unhashed leaf to verify. This should be the raw ABI encoded value.
+   * @param proof The merkle proof
+   *
+   * @returns True if the proof is valid, false otherwise
+   */
   verifyProof(root: bytes32, leaf: bytes, proof: bytes32[]): boolean {
     let hash = keccak256(keccak256(leaf));
 
