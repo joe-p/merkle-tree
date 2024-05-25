46c4b464ad0dca0dd155a350398e28b1c1e1e16a
Joe Polny <joepolny@gmail.com>
2024-05-25 08:24:35 -0400

    format json dumps

--- scripts/generate_dumps.ts
+++ scripts/generate_dumps.ts
@@ -9,5 +9,5 @@ const dumpsDir = path.join(__dirname, '..', 'test', 'dumps');
 
 const unsorted = StandardMerkleTree.of(leaves, ['string'], { sortLeaves: false });
 
-writeFileSync(path.join(dumpsDir, 'standard-v1.sorted.json'), JSON.stringify(sorted.dump()));
-writeFileSync(path.join(dumpsDir, 'standard-v1.unsorted.json'), JSON.stringify(unsorted.dump()));
+writeFileSync(path.join(dumpsDir, 'standard-v1.sorted.json'), JSON.stringify(sorted.dump(), null, 2));
+writeFileSync(path.join(dumpsDir, 'standard-v1.unsorted.json'), JSON.stringify(unsorted.dump(), null, 2));
